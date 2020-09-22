9d857fd8c482978aef2d79a7b0cded34d21177a3
Michael <michaelpadillaheredia@hotmail.com>
2020-09-22 16:50:16 -0500

    Made account controller

--- src/main/java/com/mikadifo/controllers/AccountController.java
+++ src/main/java/com/mikadifo/controllers/AccountController.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mikadifo.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 /**
  * FXML Controller class
@@ -16,12 +17,53 @@ import javafx.fxml.Initializable;
  */
 public class AccountController implements Initializable {
 
+    @FXML
+    private Button btnCancel;
+    @FXML
+    private Button btnDeleteAccount;
+    @FXML
+    private Button btnUpdate;
+    @FXML
+    private TextField txtUsername;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtLogin;
+    @FXML
+    private ComboBox<?> comboCity;
+
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void OnCancelClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnDeleteAccountClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnUptadeClick(ActionEvent event) {
+    }
+
+    @FXML
+    private void OnUsernameKeyReleased(KeyEvent event) {
+    }
+
+    @FXML
+    private void OnPasswordKeyReleased(KeyEvent event) {
+    }
+
+    @FXML
+    private void OnLoginKeyReleased(KeyEvent event) {
+    }
     
 }
