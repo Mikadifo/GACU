fdfaffb95f4d067350f669da437643933f8b7bd0
Fernanda <Fernanda@DESKTOP-AD7CHGC>
2020-09-26 21:32:12 -0500

    Registration corregida

--- src/main/java/com/mikadifo/controllers/SignUpController.java
+++ src/main/java/com/mikadifo/controllers/SignUpController.java
@@ -2,6 +2,10 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package com.mikadifo.controllers;
 
@@ -33,13 +37,12 @@ import javafx.stage.Stage;
  * @author MIKADIFO
  */
 public class SignUpController implements Initializable {
+    
+    
 
-    public SignUpController(Validations validar, UserDB user) {
-        this.validar= validar;
-        this.user= user;
-    }
-    private final UserDB user;
-    private final Validations validar;
+  
+ 
+    private  Validations validar = new Validations();
     private boolean checkedUser;
     
      Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
@@ -64,7 +67,7 @@ public class SignUpController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+//        // TODO
     }    
 
     @FXML
@@ -73,86 +76,77 @@ public class SignUpController implements Initializable {
 
     @FXML
     private void onCreateAction(ActionEvent event) {
-    try {
-            FXMLLoader loader = new FXMLLoader(SignUpController.class.getResource("/com/mikadifo/views/SignUp.fxml"));
-            Parent root = loader.load();
-
-            Scene scene = new Scene(root);
-            Stage stage = new Stage();
-            stage.initModality(Modality.APPLICATION_MODAL);
-            stage.setScene(scene);
-            Stage currentStage = (Stage) btnCreate.getScene().getWindow();
-            currentStage.close();
-            stage.show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+     //obtener los datos de el dialog y guardarlo en la base 
+        String cedula = txtLogin.getText();
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        if (validar.validateLogIn(cedula)) {
+            
+//            if (validar.validateUsername(username)) {
+//                if (validar.validatePassword(password)) {
+                    //Combo box validacion 
+                    checkUser(cedula);
+                    if (isCheckedUser()) {//comprobar en la base de datos
+                        
+                        UserDB user = new UserDB();
+                        user.setLogin(cedula);
+                        user.setPassword(password);
+                        user.setUsername(username);
+                        user.setCityId(1);
+                        user.setRoleId((short)1);
+                        user.insert();// Insertar en la base de datos
+                        
+                        alert.setHeaderText(null);
+                        alert.setTitle("Confirmación");
+                        alert.setContentText("Usuario registrado con exito");
+                    } else {
+                         alert.setHeaderText(null);
+                        alert.setTitle("Confirmación");
+                        alert.setContentText("La cedula ya está registrada");
+                    }
+//
+//                } else {
+//                    System.out.println("Contraseña incorrecta");
+//                }
+//            } else {
+//                System.out.println("Username incorrecto");
+//            }
+        } else {
+            System.out.println("No existe ");
         }
-        if (ValidacionCamposVacios()) {
-            alert.setHeaderText(null);
-            alert.setTitle("Confirmación");
-            alert.setContentText("Ingrese todos los datos");
 
-            // Si hemos pulsado en aceptar
-        } else if (isCheckedUser()) {
-             //obtener los datos de el dialog y guardarlo en la base 
-            String cedula = txtLogin.getText();
-            String username = txtUsername.getText();
-            String password = txtPassword.getText();
-
-          
-            UserDB user = new UserDB(cedula, username, password);
-            user.insert();// Insertar en la base de datos
-            alert.setHeaderText(null);
-            alert.setTitle("Confirmación");
-            alert.setContentText("Usuario registrado con exito");
-            limpiarCampos();            
-    }else{ 
-            alert.setHeaderText(null);
-            alert.setTitle("Confirmación");
-            alert.setContentText("La cedula ya está registrada");
-        }
-        }
+    }
 
     @FXML
     private void onUsernameKeyReleased(KeyEvent event) {
-         validar.validateUsername(txtUsername.getText());
+
     }
 
     @FXML
     private void onLoginKeyPressed(KeyEvent event) {
-        validar.validateLogIn(txtLogin.getText());
+
     }
 
     @FXML
     private void onPasswordKeyReleased(KeyEvent event) {
-        validar.validatePassword(txtPassword.getText());
+
     }
 
     @FXML
     private void onCityKeyRekeased(KeyEvent event) {
     }
-      private void limpiarCampos() {
-        txtLogin.setText("");
-        txtPassword.setText("");
-        txtUsername.setText("");
-       
-    }
-     private boolean ValidacionCamposVacios() {
-        return  txtUsername.getText().isEmpty() || txtLogin.getText().isEmpty()
-                || txtPassword.getText().isEmpty();
-    }
+  
      public void checkUser(String login){
-        UserDB user = new UserDB();
+       
         
+       UserDB user = new UserDB();
+
         user.setLogin(login);
         user.selectById();
-        
-        user = user.getUser();
-        
-        checkedUser = user.getLogin()==(null);
+        checkedUser = user.getUser() == (null);
             
     }
-    
+     
     public boolean isCheckedUser() {
 	return checkedUser;
     }
