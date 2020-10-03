f635a4914e5c62688ff924c3e06e57addb290ced
Fernanda <Fernanda@DESKTOP-AD7CHGC>
2020-10-02 22:26:46 -0500

    Creacion del Botton cambiar contraseña y el evento. Eliminacion de los id de los botones. cambiar eventos realeased a typed.

--- src/main/java/com/mikadifo/controllers/AccountController.java
+++ src/main/java/com/mikadifo/controllers/AccountController.java
@@ -1,15 +1,22 @@
 package com.mikadifo.controllers;
 
+import com.mikadifo.models.Roles;
 import com.mikadifo.models.db_tables.City;
 import com.mikadifo.models.db_tables.User;
 import com.mikadifo.models.table_statements.UserDB;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
@@ -24,13 +31,9 @@ public class AccountController implements Initializable {
 
     private UserDB currentUser;
 
-    
+    private WindowLoader loader;
 
-    @FXML
     private Button btnCancel;
-    @FXML
-    private Button btnDeleteAccount;
-    @FXML
     private Button btnUpdate;
     @FXML
     private TextField txtUsername;
@@ -55,27 +58,15 @@ public class AccountController implements Initializable {
         currentUser = user;
     }
 
-    @FXML
-    private void onUsernameKeyReleased(KeyEvent event) {
-    }
-
-
-    @FXML
-    private void onLoginKeyReleased(KeyEvent event) {
-    }
-
-    @FXML
     private void onCancelClick(ActionEvent event) {
         Stage currentStage = (Stage) btnCancel.getScene().getWindow();
         currentStage.close();
     }
 
-    @FXML
     private void onDeleteAccountClick(ActionEvent event) {
         getUserFromView().delete();
     }
 
-    @FXML
     private void onUptadeClick(ActionEvent event) {
         getUserFromView().update();
     }
@@ -94,5 +85,44 @@ public class AccountController implements Initializable {
 
     @FXML
     private void onCityKeyReleased(KeyEvent event) {
+    }
+
+    @FXML
+    private void onLoginKeyTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void onUsernameKeyTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void onCityKeyTyped(KeyEvent event) {
+    }
+
+    @FXML
+    private void onCancelAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void onDeleteAction(ActionEvent event) {
+        
+    }
+
+    @FXML
+    private void onUpdateAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void onChangePasswordAction(ActionEvent event) {
+        try {
+            loader = new WindowLoader();
+            loader.load("ChangePassword");
+            ChangePasswordController changePasswordController = loader.getController();
+            changePasswordController.init(loader.getScene(), null);
+
+            loader.showAndWait(true);
+        } catch (IOException ex) {
+            Logger.getLogger(MainMenuController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
