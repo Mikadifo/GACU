e7c9e21815acdddc8966af565e8f2ba12d70a8a6
Usuario <Usuario@192.168.1.54>
2020-10-11 09:02:00 -0500

    Creacion de Funciones

--- src/main/java/com/mikadifo/controllers/TriviaController.java
+++ src/main/java/com/mikadifo/controllers/TriviaController.java
@@ -2,12 +2,14 @@ package com.mikadifo.controllers;
 
 import com.mikadifo.models.Roles;
 import com.mikadifo.models.db_tables.User_Place;
+import com.mikadifo.models.function_calls.RandomTrivia;
 import com.mikadifo.models.table_statements.QuestionDB;
 import com.mikadifo.models.table_statements.UserDB;
 import com.mikadifo.models.table_statements.User_PlaceDB;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -32,6 +34,7 @@ public class TriviaController implements Initializable {
     private WindowLoader loader;
     private Stage currentStage;
     Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    private final RandomTrivia trivia = new RandomTrivia(currentUser.getId());
 
     @FXML
     private Button bntHome;
@@ -59,6 +62,7 @@ public class TriviaController implements Initializable {
     public void init(Scene scene, UserDB user) {
         btnContinue.getScene().getStylesheets().add("/styles/trivia.css");
         currentUser = user;
+//        txtQuestion.setText(trivia.selectAll().get(2));
     }
 
     @FXML
@@ -91,12 +95,18 @@ public class TriviaController implements Initializable {
 
     @FXML
     private void onContinueAction(ActionEvent event) {
+        if (trivia.selectAll().isEmpty()) {
+            boolean isOk = showAlert(Alert.AlertType.INFORMATION, null, "Ustede no ha visitado ningun lugar");
+        }else{
+        txtQuestion.setText(trivia.getQuestionContent());
+//        btnOption_1.setText(trivia.getIncorrectAnswersContents().;
         if(btnOption_1.isFocused()||btnOption_2.isFocused()||btnOption_3.isFocused()||btnOption_4.isFocused()){
             txtQuestion.setText("");
             btnOption_1.setText("");
             btnOption_2.setText("");
             btnOption_3.setText("");
             btnOption_4.setText("");
+            
            //Obtener una pregunta aleatoria basada en el place id que ha visitado el usuario
 
         } else { 
@@ -112,44 +122,49 @@ public class TriviaController implements Initializable {
 
             // cargar otra aleatoria desde la base de datosc
         // caso contrario avisar con un alert
+        }
     }
 
     private void showNewTrivia() {
-        //TABLES: Types, Questions, Answers, Question_Answer
-            /////////
-            //if place_id exist in table User_Places by userId (Si el usuario ha visitado algun lugar)
-            User_PlaceDB visitedPlaces = new User_PlaceDB();
-            List<User_PlaceDB> userVisitedPlaces;
-            List<QuestionDB> questions;
-
-            visitedPlaces.selectAll();
-            userVisitedPlaces = getVisitedPlacesByUserId(currentUser.getId()); //method in visited places todo is get place_id by user_id
-
-            if (userVisitedPlaces != null) { 
-                //Obtener un placeid aleatorio de la tabla User_Places;
-                int randomPlaceId = getRandomFromArray(userVisitedPlaces);
-                QuestionDB questionDB = new QuestionDB();
-                questionDB.selectAll();
-                questions = questionDB.getResults();
-                //Obtener una pregunta aleatoria con el mismo place_id anterior ^
-                String question = "";//set the contetn question here
-                short typeId = 0;//sets the question type id here
-                //Obtener el answer_id que corresponde al question id de la tabla QUESTION_ASNWERS
-                String correctAnswer = "";//set the content answer hereA
-                //select * from Answer join types using(type_id) where Answer.type_id != typeId and Answer.type_id (3 result only)
-                //array con todas las 4 respuestas here
-                //[1][2][3][4]
-                //mezclar [4][1][2][3]
-                //for botones asginar el shuffled array
-            //else alert de que aun no ha visitado un lugar
-            } else {
-                alert.setHeaderText(null);
-                alert.setTitle("Aviso");
-                alert.setContentText("Aún no ha visitado ningun lugar. No puede acceder a la trivia");
-                alert.showAndWait();
-            }
+        
+//        //TABLES: Types, Questions, Answers, Question_Answer
+//            /////////
+//            //if place_id exist in table User_Places by userId (Si el usuario ha visitado algun lugar)
+////            User_PlaceDB visitedPlaces = new User_PlaceDB();
+////            List<User_PlaceDB> userVisitedPlaces;
+////            List<QuestionDB> questions;
+////
+////            visitedPlaces.selectAll();
+////            userVisitedPlaces = getVisitedPlacesByUserId(currentUser.getId()); //method in visited places todo is get place_id by user_id
+//
+//            if (userVisitedPlaces != null) { 
+//                //Obtener un placeid aleatorio de la tabla User_Places;
+//                int randomPlaceId = getRandomFromArray(userVisitedPlaces);
+//                QuestionDB questionDB = new QuestionDB();
+//                questionDB.selectAll();
+//                questions = questionDB.getResults();
+//                //Obtener una pregunta aleatoria con el mismo place_id anterior ^
+//                String question = "";//set the contetn question here
+//                short typeId = 0;//sets the question type id here
+//                //Obtener el answer_id que corresponde al question id de la tabla QUESTION_ASNWERS
+//                String correctAnswer = "";//set the content answer hereA
+//                //select * from Answer join types using(type_id) where Answer.type_id != typeId and Answer.type_id (3 result only)
+//                //array con todas las 4 respuestas here
+//                //[1][2][3][4]
+//                //mezclar [4][1][2][3]
+//                //for botones asginar el shuffled array
+//            //else alert de que aun no ha visitado un lugar
+//            } else {
+//                alert.setHeaderText(null);
+//                alert.setTitle("Aviso");
+//                alert.setContentText("Aún no ha visitado ningun lugar. No puede acceder a la trivia");
+//                alert.showAndWait();
+//            }
     }
-
+//    private String getRandomIncorrectContentAnswere(){
+//            
+//    }
+    
     private List<User_PlaceDB> getVisitedPlacesByUserId(int userId) {
         return null;
         //TODO
