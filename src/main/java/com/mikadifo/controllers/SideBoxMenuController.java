7a5a40d032a117c3f35754498d1581ed25eb5f4e
Michael <michaelpadillaheredia@hotmail.com>
2020-10-13 18:16:18 -0500

    HambuguerMenu ready

--- src/main/java/com/mikadifo/controllers/SideBoxMenuController.java
+++ src/main/java/com/mikadifo/controllers/SideBoxMenuController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mikadifo.controllers;
 
 import java.net.URL;
@@ -31,35 +26,16 @@ public class SideBoxMenuController implements Initializable {
     }    
 
     @FXML
-    private void onMenuAction(ActionEvent event) {
+    private void onUserRolesReportAction(ActionEvent event) {
     }
 
     @FXML
-    private void onViewAction(ActionEvent event) {
+    private void onCompletedQuizzesAction(ActionEvent event) {
     }
 
     @FXML
-    private void onEnter_Action(ActionEvent event) {
+    private void onVisitedPlacesAction(ActionEvent event) {
     }
 
-    @FXML
-    private void onUpdate_Action(ActionEvent event) {
-    }
-
-    @FXML
-    private void onDelete_Action(ActionEvent event) {
-    }
-
-    @FXML
-    private void onReportsAction(ActionEvent event) {
-    }
-
-    @FXML
-    private void onFAQAction(ActionEvent event) {
-    }
-
-    @FXML
-    private void onHelpAction(ActionEvent event) {
-    }
     
 }
