73803b39ea5551ad54f02b739167b069bb338eb5
Fernanda <Fernanda@DESKTOP-AD7CHGC>
2020-10-14 09:57:04 -0500

    BD

--- src/main/java/com/mikadifo/models/DB_Connection.java
+++ src/main/java/com/mikadifo/models/DB_Connection.java
@@ -11,9 +11,9 @@ public class DB_Connection {
     private Connection connection;
     private PreparedStatement statement;
 
-    private String url = "jdbc:postgresql://localhost:5432/GaCu";
+    private String url = "jdbc:postgresql://localhost:5432/gacu_db";
     private String login = "postgres"; 
-    private String password = "1234";
+    private String password = "admin";
     
     public DB_Connection() {
 	loadDriver();
